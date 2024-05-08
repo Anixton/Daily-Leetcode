ef8332630a4dc595ab2e734edd62808633916b55
Hakan Gezginci <121567782+Anixton@users.noreply.github.com>
2024-05-08 20:14:33 +0300

    Time: 4 ms (84.39%), Space: 49.9 MB (87.73%) - LeetHub

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.java
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.java
@@ -1,7 +1,7 @@
 class Solution {
     public int maxScoreSightseeingPair(int[] values) {
-        int max_i = values[0] + 0; // max value of values[i] + i
-        int max_score = Integer.MIN_VALUE; // max score of a sightseeing pair
+        int max_i = values[0] + 0; 
+        int max_score = Integer.MIN_VALUE; 
         
         for(int j = 1; j < values.length; j++) {
             max_score = Math.max(max_score, max_i + values[j] - j);
