68aeb0919c4c701f7a04f4bac2ed38fa9d11998a
Hakan Gezginci <121567782+Anixton@users.noreply.github.com>
2024-05-31 07:52:25 +0300

    Time: 6 ms (31.65%), Space: 45.3 MB (81.95%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
-        HashMap<Integer,Integer> countOfNums = new HashMap<Integer,Integer>();
-        
+        HashMap<Integer, Integer> countOfNums = new HashMap<Integer, Integer>();
+
         for (int i = 0; i < nums.length; i++) {
             if (!countOfNums.containsKey(nums[i])) {
                 countOfNums.put(nums[i], 1);
@@ -10,17 +10,18 @@ class Solution {
                 countOfNums.put(nums[i], count + 1);
             }
         }
-        
+
         int[] result = new int[2];
-        int index=0;
-        
+        int index = 0;
+
         for (Map.Entry<Integer, Integer> entry : countOfNums.entrySet()) {
-            if(entry.getValue()==1){
-                result[index++]=entry.getKey();
-                if(index==2) return result;
+            if (entry.getValue() == 1) {
+                result[index++] = entry.getKey();
+                if (index == 2)
+                    return result;
             }
         }
-        
+
         return result;
     }
 }
