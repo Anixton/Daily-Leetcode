28281cbfc3981b4779c237f384b8afb4e9f6a071
Hakan Gezginci <121567782+Anixton@users.noreply.github.com>
2024-08-10 16:39:04 +0300

    Time: 10 ms (11.96%), Space: 44.6 MB (23.43%) - LeetHub

--- 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.java
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.java
@@ -4,17 +4,8 @@ class Solution {
         HashMap<Integer, Integer> arrMap = new HashMap<Integer, Integer>();
         
         for(int index = 0; index < target.length; index++) {
-            if(targetMap.containsKey(target[index])) {
-                targetMap.put(target[index], targetMap.get(target[index]) + 1);
-            } else {
-                targetMap.put(target[index], 1);
-            }
-            
-            if(arrMap.containsKey(arr[index])) {
-                arrMap.put(arr[index], arrMap.get(arr[index]) + 1);
-            } else {
-                arrMap.put(arr[index], 1);
-            }
+            targetMap.put(target[index], targetMap.getOrDefault(target[index], 0) + 1);
+            arrMap.put(arr[index], arrMap.getOrDefault(arr[index], 0) + 1);
         }
         
         return arrMap.equals(targetMap);
